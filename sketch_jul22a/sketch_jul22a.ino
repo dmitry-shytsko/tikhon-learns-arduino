7bfb9e84a7bba9db3899b14c26cef21aeda73cad
Dmitry Shytsko <dmitry.shytsko@gmail.com>
2023-07-25 17:56:29 +0300

    Added the very first sketch

---
+++ sketch_jul22a/sketch_jul22a.ino
@@ -0,0 +1,121 @@
+int ledpins[] = {5, 6, 9, 10};
+const int ledCount = sizeof(ledpins) / sizeof(int);
+int inputpin = 12;
+int statuspin = 2;
+
+int intensity = 0;
+int delta = 1;
+int maxIntensity = 20;
+const int overflowIntensity = 100;
+
+bool inputEventPending = false;
+bool inputEventProcessed = false;
+
+void setup() {
+  pinMode(inputpin, INPUT);
+  pinMode(statuspin, OUTPUT);
+  for (int i = 0; i < ledCount; i++) {
+    pinMode(ledpins[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+
+void nextMode() {
+  maxIntensity += 20;
+
+  if (maxIntensity > overflowIntensity) {
+    maxIntensity = 0;
+  }
+
+  modeChange();    
+}
+
+void prevMode() {
+  maxIntensity -= 20;
+
+  if (maxIntensity < 0) {
+    maxIntensity = 100;  
+  }
+
+  modeChange();    
+}
+
+void modeChange() {
+    intensity = 0;
+    delta = 1;
+
+    if (maxIntensity == 0) {
+      Serial.println("no blink mode");
+    } else {
+      Serial.print("maxIntensity:");
+      Serial.println(maxIntensity);        
+    }
+}
+
+void statusBlink() {
+  digitalWrite(statuspin, HIGH);
+  delay(200);
+  digitalWrite(statuspin, LOW);
+  delay(50);
+}
+
+void nextStepBlink() {
+  //Main logic: sending voltage to LEDs
+  for (int i = 0; i < ledCount; i++) {
+    analogWrite(ledpins[i], map(intensity, 0, 100, 0, 255));      
+  }
+  delay(25);
+
+  if (intensity == maxIntensity && delta == 1) {
+    delta = -1;
+  } else if (intensity == 0 && delta == -1) {
+    delta = 1;
+  } else {
+    intensity += delta;
+  }
+}
+
+void nextStepNoBlink() {
+  //Main logic: sending voltage to LEDs
+  for (int i = 0; i < ledCount; i++) {
+    analogWrite(ledpins[i], map(100, 0, 100, 0, 255));      
+  }
+  delay(25);
+}
+
+void loop() {
+  if (digitalRead(inputpin)==LOW) {
+    inputEventPending = false;
+  } else {
+    if (!inputEventPending) {
+      inputEventPending = true;
+      inputEventProcessed = false;
+      statusBlink();
+    }
+  }
+
+  if (!inputEventProcessed) {
+    inputEventProcessed = true;
+    nextMode();
+  }
+
+  while (Serial.available()){
+    char nextChar = Serial.read();
+
+    if (nextChar == '+') {
+      nextMode();
+      statusBlink();
+    }
+
+    if (nextChar == '-') {
+      prevMode();
+      statusBlink();
+    }
+  }
+
+  if (maxIntensity == 0) {
+    nextStepNoBlink();
+  } else {
+    nextStepBlink();
+  }
+}
